409995f6356d92badb686b86f3383718008b725a
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2026-05-23 22:09:27 -0300

    feat: modelo para criação de cliente pipefy

---
+++ app/integrations/pipefy_client.py
@@ -0,0 +1,34 @@
+import json
+
+
+class PipefyClient:
+
+    @staticmethod
+    def simulate_create_card(
+        nome: str, email: str, valor_patrimonio: float
+    ):
+        mutation = """
+        mutation CreateCard($input: CreateCardInput!) {
+          createCard(input: $input) {
+            card {
+              id
+              title
+            }
+          }
+        }
+        """
+        variables = {
+            "input": {
+                "pipe_id": "234440912",  # supondo que seja o id do pipe dentro do pipefy
+                "title": f"Análise - {nome}",
+                "fields_attributes": [
+                    {"field_id": "cliente_email", "field_value": email},
+                    {"field_id": "valor_patrimonio", "field_value": str(valor_patrimonio)},
+                ],
+            }
+        }
+
+        # Retorna a string formatada para simular o payload que seria enviado no corpo HTTP
+        return json.dumps(
+            {"query": mutation, "variables": variables}, indent=2, ensure_ascii=False
+        )
